e24151e21894f9f1752f1799892345ed78927e9c
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2023-10-31 22:29:55 +0600

    initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
+const defaultColors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            ...defaultColors,
+            darkPrimary: "#827ebe",
+            darkSecondary: "#1e1f0f",
+            darkAccent: "#5954a6",
+            darkText: "#f8f8f1",
+            darkBg: "#0d0e07",
+
+            lightPrimary: "#618afa",
+            lightSecondary: "#eff0e0",
+            lightAccent: "#5e59ab",
+            lightText: "#0e0e07",
+            lightBg: "#EDFBFB",
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
